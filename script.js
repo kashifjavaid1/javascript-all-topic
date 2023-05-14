26061749bfff4296f68cfae2e1e1cce279dd9bf3
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-14 09:48:10 +0500

    condational operatores and if condation
    condation check

--- script.js
+++ script.js
@@ -11,5 +11,12 @@
 // var ab=12;
 // var bc=22;
 // alert(ab+bc);
-
+// condtional operaters
+var user=+prompt("user enter a number");
+if (user>50) {
+    console.log("correct value")
+}
+else{
+    console.log("not correct value");
+}
 
