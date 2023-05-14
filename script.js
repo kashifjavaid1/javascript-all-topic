5a76278b9e62dbce17857d9693f93ac9977e0450
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-14 09:52:42 +0500

    if statment
    if and else statement

--- script.js
+++ script.js
@@ -12,11 +12,21 @@
 // var bc=22;
 // alert(ab+bc);
 // condtional operaters
-var user=+prompt("user enter a number");
-if (user>50) {
-    console.log("correct value")
-}
-else{
-    console.log("not correct value");
-}
+// var user=+prompt("user enter a number");
+// if (user>50) {
+//     console.log("correct value")
+// }
+// else{
+//     console.log("not correct value");
+// }
+
+// if statment
+// var user1=+prompt("user1 enter a number");
+// var user2=+prompt("user2 enter a number")
+// if (user1==user2) {
+//     console.log("equal values both users");
+// }
+// else{
+//     console.log("not equal values both users");
+// }
 
