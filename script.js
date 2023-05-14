2ebad3fd2e58e397d2c7e9cfd34884b3f3694dec
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-14 09:40:28 +0500

    string
    varaible

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // variabel create
-var a=5;
-var b=10;
-var result=a+b;
-console.log(result);
+// var a=5;
+// var b=10;
+// var result=a+b;
+// console.log(result);
+//  string variable
+var a="kashif";
+alert(a);
